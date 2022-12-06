f09972e5db67529a7a71c65a0573a4045fb1f7eb
github-Denis-by <lexad83@gmail.com>
2022-12-06 23:36:56 +0200

    feat: add function create

--- src/fs/create.js
+++ src/fs/create.js
@@ -1,5 +1,22 @@
+import { writeFile } from 'node:fs/promises';
+import { dirname, join } from 'node:path';
+import { fileURLToPath } from 'node:url';
+
+const __filename = fileURLToPath(import.meta.url); //путь к файлу create.js D:\NODEJS\node-nodejs-basics\src\fs\create.js
+const __dirname = dirname(__filename); //путь к родительской папке (тоесть fs) D:\NODEJS\node-nodejs-basics\src\fs
+
 const create = async () => {
-    // Write your code here 
+    const src = join(__dirname, 'files', 'fresh.txt') //join - склеивает две части пути
+    const textContent = 'I am fresh and young';
+    const textError = 'FS operation failed';
+
+    try { 
+        await  writeFile(src, textContent,  { flag: 'wx' });
+    }
+    catch (err) {
+        throw new Error(textError);
+
+    }    
 };
 
-await create();
+create();
